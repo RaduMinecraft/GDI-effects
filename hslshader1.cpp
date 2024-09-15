0bea014bbd7d41c38c914d1ecefdfe159dbb1cbd
RaduMinecraft <131392805+RaduMinecraft@users.noreply.github.com>
2024-09-15 20:11:43 +0300

    Add files via upload

---
+++ hslshader1.cpp
@@ -0,0 +1,102 @@
+#include <windows.h>
+#include <MATH.h>
+
+COLORREF COLORHSL(int length) {
+    double h = fmod(length, 360.0);
+    double s = 1.0;
+    double l = 0.5;
+
+    double c = (1.0 - fabs(2.0 * l - 1.0)) * s;
+    double x = c * (1.0 - fabs(fmod(h / 60.0, 2.0) - 1.0));
+    double m = l - c / 2.0;
+
+    double r1, g1, b1;
+    if (h < 60) {
+        r1 = c;
+        g1 = x;
+        b1 = 0;
+    }
+    else if (h < 120) {
+        r1 = x;
+        g1 = c;
+        b1 = 0;
+    }
+    else if (h < 180) {
+        r1 = 0;
+        g1 = c;
+        b1 = x;
+    }
+    else if (h < 240) {
+        r1 = 0;
+        g1 = x;
+        b1 = c;
+    }
+    else if (h < 300) {
+        r1 = x;
+        g1 = 0;
+        b1 = c;
+    }
+    else {
+        r1 = c;
+        g1 = 0;
+        b1 = x;
+    }
+
+    int red = static_cast<int>((r1 + m) * 255);
+    int green = static_cast<int>((g1 + m) * 255);
+    int blue = static_cast<int>((b1 + m) * 255);
+
+    return RGB(red, green, blue);
+}
+
+typedef union COLOR {
+    COLORREF rgb;
+    struct {
+        BYTE blue;
+        BYTE green;
+        BYTE red;
+    };
+} COLOR;
+
+DWORD WINAPI shader4(LPVOID lpParam) {
+    int x = GetSystemMetrics(SM_CXSCREEN);
+    int y = GetSystemMetrics(SM_CYSCREEN);
+    int j = 1;
+    int c = 0;
+
+    COLOR* data = (COLOR*)VirtualAlloc(0, (x * y + x) * sizeof(COLOR), MEM_COMMIT, PAGE_READWRITE);
+    HDC hdc = GetDC(0);
+    HDC mdc = CreateCompatibleDC(hdc);
+    HBITMAP bmp = CreateBitmap(x, y, 1, 32, data);
+    SelectObject(mdc, bmp);
+
+    while (true) {
+        BitBlt(mdc, 0, 0, x, y, hdc, 0, 0, SRCCOPY);
+        GetBitmapBits(bmp, x * y * sizeof(COLOR), data);
+
+        for (int x2 = 0; x2 < x; x2++) {
+            for (int y2 = 0; y2 < y; y2++) {
+                int j = c * 4;
+
+                //int wave = (int)(j + (j * sin(sqrt((x2 * x2) + (y2 * y2)) / 64)));
+                //int wave = (int)(j + (j * sin(((x2 * x2) + (y2 * y2)) / 64)));
+                //int wave = (int)(j + (j * sin(((x2 * x2) - (y2 * y2)) / 64)));
+                int wave = (int)(j + (j * cbrt(((x2 * x2) - (y2 * y2)) / 64)));
+
+                data[y2 * x + x2].rgb = COLORHSL((x * y) + wave);
+            }
+        }
+        c++;
+
+        SetBitmapBits(bmp, x * y * sizeof(COLOR), data);
+        BitBlt(hdc, 0, 0, x, y, mdc, 0, 0, SRCCOPY);
+
+        Sleep(10);
+    }
+}
+
+int main() {
+    CreateThread(0, 0, shader4, 0, 0, 0);
+    Sleep(-1);
+    return 0;
+}
